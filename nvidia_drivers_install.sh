ef460e2de172d42ad95a66315de1f7cbc22bd136
danielr <daniel.ratmiroff@sdui.de>
2023-07-05 10:30:02 +0200

    daily update

--- nvidia_drivers_install.sh
+++ nvidia_drivers_install.sh
@@ -1,76 +1,88 @@
 #!/bin/bash
 
-# migrate this to python
-
+# This script is for installing Nvidia drivers on Ubuntu 20.04
 set -e
 
-# Install Nvidia drivers 
+# ----------------
+# Variables
+# ----------------
+DRIVER_FILE=$HOME/system/drivers/NVIDIA*.run
 
-echo "# Updating the system"
-sudo apt update -y
-sudo apt upgrade -y
+# ----------------
+# Help section
+# ----------------
+help_function() {
+  echo "# Usage: ./nvidia_drivers_install.sh [step]"
+  echo "# Step 1: Install from source"
+  echo "# Step 2: Disable display manager"
+  echo "# Step 3: Install the driver"
+}
 
-echo "# Current Nvidia driver installed:"
-nvidia-smi
+cmdline_space() {
+  echo "-------------------"
+}
 
-if [ $# -eq 0 ]; then
-  echo "# No step argument supplied, starting from step 1..."
-  fi
+# ----------------
+# Step 1 
+# ----------------
+execute_step_one() {
+  cmdline_space
+  echo "Installing Nvidia drivers"
+  read -r -p "# Do you want to install from source? (y/n) -- (not recommended)" install_source
 
-  elif [ $1 -eq 1 ]; then
+  if [ $install_source == "y" ]; then
     echo "# Installing from source"
     install_from_source
-
-  elif [ $1 -eq 2 ]; then
-    echo "# Installing from apt"
-    sudo ubuntu-drivers autoinstall
-    echo "# Ready to reboot"
-    exit 0
-
   else
-    echo "# Wrong argument supplied"
-    echo "# Please use 1 or 2"
-    exit 1
+    echo "# Installing automatically"
+    auto_install
   fi
 
-step_one() {
-    read -r "# Do you want to install from source? (y/n)" source
+  exit 0
+}
 
-    if [ $source == "y" ]; then
-      echo "# Installing from source"
-      install_from_source
-      else
-        echo "# Autoinstalling from apt"
-        sudo ubuntu-drivers autoinstall
-        echo "# Ready to reboot"
-        exit 0
-    exit 0
+auto_install() {
+  cmdline_space
+  echo "# Checking displays"
+  sudo lshw -c display
+
+  echo "# Adding repository"
+  sudo add-apt-repository ppa:graphics-drivers
+  sudo apt-get update -y
+
+  echo "# Checking devices and installing the driver"
+  ubuntu-drivers devices
+  sudo ubuntu-drivers autoinstall
 }
 
 install_from_source() {
-  lspci -nn | grep VGA
-  find_driver()
+  lspci -nn | grep VGA # Check nvidia model
 
-  echo "# Disabling Nouveau"
-  sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-  sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+  cmdline_space
+  is_driver_available
 
-  echo "# Updating initial RAM filesystem"
-  sudo update-initramfs -u
+  configure_os_before_install
 
-
-  echo "### Step 1/3 is done ###"
-  echo "# Reboot and run the script again"
-  echo "# with number 2 as argument!!"
+  cmdline_space
+  echo "### Step 1/3 ###"
   echo "# Ready to reboot"
 }
 
-find_driver() {
-  ls -l $HOME/system/drivers/NVIDIA*.run
+configure_os_before_install() {
+  echo "# Disabling nouveau"
 
-  if $? -eq 0; then
+  sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+  sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+
+  echo "# Regenerating initial RAM filesystem"
+  sudo update-initramfs -u
+}
+
+is_driver_available() {
+  files=( $DRIVER_FILE )
+
+  if [ -e "${files[0]}" ]; then
     echo "# Driver found"
-    exit 0
 
   else
     echo "# Driver not found"
@@ -78,3 +90,59 @@ find_driver() {
     exit 1
   fi
 }
+
+# ----------------
+# Step 2
+# ----------------
+execute_step_two() {
+  cmdline_space
+  echo "# Disabling display manager"
+  echo "##### CAUTION: Rebooting will be necessary #####"
+  sleep 5
+  sudo systemctl stop display-manager.service
+  exit 0
+}
+
+# ----------------
+# Step 3
+# ----------------
+execute_step_three() {
+  cmdline_space
+  echo "# Starting installation..."
+  sleep 3
+  sudo sh $DRIVER_FILE
+  exit 0
+}
+
+# ----------------
+# Script
+# ----------------
+if [ $# -eq 0  ]; then
+  echo "### No step argument provided, please provide a step number"
+  help_function
+fi
+
+if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+  echo "# This script is for installing Nvidia drivers on Ubuntu 20.04"
+  help_function
+  exit 0
+fi
+
+if (( $1 == 1)); then
+  echo "# Updating the system"
+  sudo apt update -y
+  sudo apt upgrade -y
+
+  execute_step_one
+
+elif (( $1 == 2)); then
+  execute_step_two
+
+elif (( $1 == 3)); then
+  execute_step_three
+
+else
+  echo "# Wrong argument supplied"
+  help_function
+  exit 1
+fi
