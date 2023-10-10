47afaf05df09735a09d0d82fd225bc388f834b73
danielr <daniel.ratmiroff@sdui.de>
2023-10-10 10:30:01 +0200

    daily update

--- start_work_apps.sh
+++ start_work_apps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Arrange windows to start work
-#
+URLS=("https://www.youtube.com/watch?v=nMfPqeZjc2c&t=10622s&ab_channel=RelaxingWhiteNoise" "https://chat.openai.com/?model=gpt-4")
+
 MESSAGER_APP="slack"
 MESSAGER_APP_PATH="/snap/bin/$MESSAGER_APP"
 
@@ -12,6 +13,32 @@ move_to_main_screen() {
 	# Ensure windows are in left monitor
 	xdotool keydown Super keydown Shift key Left keyup Shift keyup Super
 }
+
+move_to_secondary_screen() {
+	# Ensure windows are in right monitor
+	xdotool keydown Super keydown Shift key Right keyup Shift keyup Super
+}
+
+open_helper_browser() {
+	for url in "${URLS[@]}"; do
+		brave-browser --new-tab "$url" &
+		sleep 1
+	done
+
+	move_to_secondary_screen
+	sleep 1
+	xdotool keydown Super key Up keyup Super
+}
+
+open_helper_browser() {
+	for url in "${URLS[@]}"; do
+		brave-browser --new-tab "$url" &
+		sleep 1
+	done
+}
+
+open_helper_browser
+sleep 1
 
 # Start apps
 $MESSAGER_APP_PATH
