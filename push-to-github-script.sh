6ca5176844b7478e10f3f20aae2988dc562de8e1
danielr <daniel.ratmiroff@sdui.de>
2024-05-23 20:59:01 +0200

    daily update

--- push-to-github-script.sh
+++ push-to-github-script.sh
@@ -3,17 +3,22 @@ set -x
 
 # Script used by cronjobs to push to github automatically
 
-export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/github_cronjob.pub
 
 # Date in format Day-Month-Year
 date=$(date +"%Y-%m-%d %T")
 
-# Commit message
 message="daily update"
-folder="$1"
+folder="$1" # The folder to push to github
 
 cd $1
 git add .
 git commit -m "${message}"
 git push -u origin master
+
+# Kill the ssh-agent
+kill $SSH_AGENT_PID
+
 exit 0
