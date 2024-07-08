64806f789f30edac2d7e0eb845745bfccb199a74
danielr <daniel.ratmiroff@sdui.de>
2024-07-08 11:30:01 +0200

    daily update

--- get_going_script_on_startup.py
+++ get_going_script_on_startup.py
@@ -3,48 +3,51 @@ import requests
 import time
 import datetime
 
-# Global variables
-script_paths = "/home/daniel/automation/"
 
-print("--------------------------------------------")
-print("Hello my dear Daniel, I'm here for you\n")
+subprocess.run(['neofetch'])
 
-# Check if today is a weekend
-today = datetime.datetime.today().weekday()
-if today >= 5:  # 5 = Saturday, 6 = Sunday
-    print("It's weekend! Wanna work? (yes/y) default is no")
-    choice = input("Your choice: ").strip().lower()
-    if choice not in ['yes', 'y']:
-        print("Enjoy your weekend! Love you!")
-        exit()
-
-print("Let's work!")
-print("I'm gonna open your work applications for you.")
-
-# Show a loading timer of 5 seconds
-timer = 15
-print(f"Starting in {timer} seconds...")
-for i in range(timer, 0, -1):
-    print(f"{'*' * (6 - i)} {i}...")
-    time.sleep(1)
-
-print("Starting work...")
-subprocess.run(['fish', '-c', 'work start'])
-
-url = "https://api.quotable.io/random"
-response = requests.get(url)
-if response.status_code == 200:
-    print("--------------------------------------------")
-    print("----------------------------------")
-    print("------------------------")
-    print("--------------")
-    print("-----")
-    print(response.json()['content'])
-    print("- " + response.json()['author'])
-    print("-----")
-    print("--------------")
-    print("------------------------")
-    print("----------------------------------")
-    print("--------------------------------------------")
-
+# # Global variables
+# script_paths = "/home/daniel/automation/"
+#
+# print("--------------------------------------------")
+# print("Hello my dear Daniel, I'm here for you\n")
+#
+# # Check if today is a weekend
+# today = datetime.datetime.today().weekday()
+# if today >= 5:  # 5 = Saturday, 6 = Sunday
+#     print("It's weekend! Wanna work? (yes/y) default is no")
+#     choice = input("Your choice: ").strip().lower()
+#     if choice not in ['yes', 'y']:
+#         print("Enjoy your weekend! Love you!")
+#         exit()
+#
+# print("Let's work!")
+# print("I'm gonna open your work applications for you.")
+#
+# # Show a loading timer of 5 seconds
+# timer = 15
+# print(f"Starting in {timer} seconds...")
+# for i in range(timer, 0, -1):
+#     print(f"{'*' * (6 - i)} {i}...")
+#     time.sleep(1)
+#
+# print("Starting work...")
+# subprocess.run(['fish', '-c', 'work start'])
+#
+# url = "https://api.quotable.io/random"
+# response = requests.get(url)
+# if response.status_code == 200:
+#     print("--------------------------------------------")
+#     print("----------------------------------")
+#     print("------------------------")
+#     print("--------------")
+#     print("-----")
+#     print(response.json()['content'])
+#     print("- " + response.json()['author'])
+#     print("-----")
+#     print("--------------")
+#     print("------------------------")
+#     print("----------------------------------")
+#     print("--------------------------------------------")
+#
 print("Have a good day! Love you!")
