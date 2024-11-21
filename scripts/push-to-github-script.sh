76d9282710713c334fe4e37c10a87df30abd78e3
danielr <daniel.ratmiroff@sdui.de>
2024-11-21 09:07:48 +0100

    Remove outdated cronjob playbook and update script to include SSH key path

--- scripts/push-to-github-script.sh
+++ scripts/push-to-github-script.sh
@@ -3,9 +3,16 @@ set -x
 
 # Script used by cronjobs to push to github automatically
 
+if [ -z "$2" ]; then
+    echo "Usage: $0 <folder> <ssh-key-path>"
+    exit 1
+fi
+
+ssh_key_path="$2"
+
 eval "$(ssh-agent -s)"
 
-ssh-add ~/.ssh/github_cron
+ssh-add "$ssh_key_path"
 
 export SSH_AUTH_SOCK
 
@@ -15,7 +22,7 @@ date=$(date +"%Y-%m-%d %T")
 message="daily update"
 folder="$1" # The folder to push to github
 
-cd $1
+cd "$folder" || exit
 git add .
 git commit -m "${message}"
 git push -u origin master
